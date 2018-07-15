b377f6440d7bcdf2eb6f9352f6f1c202ccedcbd3
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-07-15 16:58:53 -0600

    creates the message class and the connect_four runner.

---
+++ lib/connect_four.rb
@@ -0,0 +1,8 @@
+require './lib/board.rb'
+require './lib/message.rb'
+
+
+message = Message.new
+message.welcome
+board = Board.new
+board.print
---
+++ lib/message.rb
@@ -0,0 +1,16 @@
+class Message
+
+def welcome
+  puts "++++++Welcome to Connect Four++++++"
+  puts "Your goal is to connect four of you pieces before your opponet."
+  puts "Your checkers will be represented by 'X's'."
+  puts "Your opponet's checkers will be represented by 'O's'."
+  puts "Good luck!"
+
+end
+
+
+
+
+
+end
